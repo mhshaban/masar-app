870a173308bd3d8b6dc82f77ece398b6048721d3
countryclubequestrian <m.h.shaban@gmail.com>
2026-08-29 23:36:09 +0300

    Activate synchronized app cache v3

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "masar-static-v2";
+const CACHE_VERSION = "masar-static-v3";
 const STATIC_CACHE = CACHE_VERSION;
 const APP_SCOPE = new URL(self.registration.scope);
 const staticUrl = (path) => new URL(path, APP_SCOPE).href;
