30d9c993c591fe17e878a19092c7d0364601f38f
countryclubequestrian <m.h.shaban@gmail.com>
2026-09-01 10:57:10 +0300

    Publish calmer daily priorities layout

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "masar-static-v14";
+const CACHE_VERSION = "masar-static-v15";
 const STATIC_CACHE = CACHE_VERSION;
 const APP_SCOPE = new URL(self.registration.scope);
 const staticUrl = (path) => new URL(path, APP_SCOPE).href;
